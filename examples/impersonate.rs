c42bdfee96ea3bb0524c3f3d322d463fe2362762
0x676e67 <gngppz@gmail.com>
2024-07-27 22:02:22 +0800

    feat(impersonate): Optimize reuse of impersonate configuration

--- examples/impersonate.rs
+++ examples/impersonate.rs
@@ -6,13 +6,16 @@ use std::error::Error;
 async fn main() -> Result<(), Box<dyn Error>> {
     // Build a client to mimic Edge127
     let client = rquest::Client::builder()
-        .impersonate(Impersonate::Edge127)
+        .impersonate(Impersonate::Safari17_5)
         .enable_ech_grease()
         .permute_extensions()
         .cookie_store(true)
         .build()?;
 
     // Use the API you're already familiar with
+    let resp = client.get("https://tls.peet.ws/api/all").send().await?;
+    println!("{}", resp.text().await?);
+
     let resp = client.get("https://tls.peet.ws/api/all").send().await?;
     println!("{}", resp.text().await?);
 
